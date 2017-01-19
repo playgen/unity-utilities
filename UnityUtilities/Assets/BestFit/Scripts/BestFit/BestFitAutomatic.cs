bfc903146407eea603459ba5bcf2425df1023879
Felix <felix@playgen.com>
2017-01-19 11:56:58 +0000

    add best fit automatic script to set child best fit text objects to the same size On Enable Add Font replace script to easily replace fonts Add shortcuts to add custom shortcuts, currently contains UI shortcuts

---
+++ UnityUtilities/Assets/BestFit/Scripts/BestFit/BestFitAutomatic.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Add this class as a component of an object that you want children with best fit to all be the same size
+/// </summary>
+public class BestFitAutomatic : MonoBehaviour
+{
+    private List<Text> _bestFitChildren = new List<Text>();
+
+    void OnEnable()
+    {
+        var children = transform.GetComponentsInChildren<Text>();
+        if (_bestFitChildren.Count == 0)
+        {
+            _bestFitChildren = children.Where(child => child.resizeTextForBestFit).ToList();
+        }
+
+        var smallestFontSize = 0;
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform);
+
+        foreach (var text in _bestFitChildren)
+        {
+            text.resizeTextForBestFit = true;
+
+            text.cachedTextGenerator.Invalidate();
+            text.cachedTextGenerator.Populate(text.text, text.GetGenerationSettings(text.rectTransform.rect.size));
+
+            text.resizeTextForBestFit = false;
+
+            var newSize = text.cachedTextGenerator.fontSizeUsedForBestFit;
+            var newSizeRescale = text.rectTransform.rect.size.x / text.cachedTextGenerator.rectExtents.size.x;
+            if (text.rectTransform.rect.size.y / text.cachedTextGenerator.rectExtents.size.y < newSizeRescale)
+            {
+                newSizeRescale = text.rectTransform.rect.size.y / text.cachedTextGenerator.rectExtents.size.y;
+            }
+
+            newSize = Mathf.FloorToInt(newSize * newSizeRescale);
+            if (newSize < smallestFontSize || smallestFontSize == 0)
+            {
+                smallestFontSize = newSize;
+            }
+        }
+
+        foreach (var text in _bestFitChildren)
+        {
+            text.fontSize = smallestFontSize+1;
+        }
+    }
+}
---
+++ UnityUtilities/Assets/Editor/FontReplace.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FontReplace : EditorWindow {
+
+    private readonly List<Font> _gameFonts = new List<Font>();
+	private List<string> _names = new List<string>();
+
+    [MenuItem("Tools/Replace Fonts")]
+	public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(FontReplace));
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Replace selected fonts", EditorStyles.boldLabel);
+	    if (GUILayout.Button("Refresh") || _gameFonts.Count == 0)
+	    {
+		    GUILayout.Space(10f);
+			_names = FontNames();
+	    }
+		for (var i = 0; i < _names.Count; i++)
+		{
+			//list all the fonts
+			if (_gameFonts.Count == 0 || _gameFonts.Count < i + 1)
+				_gameFonts.Add(new Font());
+			EditorGUILayout.BeginHorizontal();
+			GUILayout.Label(_names[i]);
+			if (GUILayout.Button("Replace With"))
+			{
+				ReplaceFont(_names[i], _gameFonts[i]);
+			}
+			_gameFonts[i] = (Font)EditorGUILayout.ObjectField(_gameFonts[i], typeof(Font), true);
+			//create a button to replace the selected font with the font dragged in
+
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+    void ReplaceFont(string name, Font font)
+    {
+        //check the font has been set to a valid font
+        if (font == null || font.fontNames.Length == 0)
+            return;
+        //now get all text types in the scene and change the ones with a matching font name to the new font
+        var allTexts = Resources.FindObjectsOfTypeAll<UnityEngine.UI.Text>();
+        foreach (var t in allTexts)
+        {
+            if (t.font.ToString() == name)
+            {
+                t.font = font;
+            }
+        }
+    }
+    List<string> FontNames()
+    {
+        //return a string of font names
+        var fontNames = new List<string>();
+        var allTexts = Resources.FindObjectsOfTypeAll<UnityEngine.UI.Text>();
+
+        foreach(var t in allTexts)
+        {
+            if (!fontNames.Contains(t.font.ToString()))
+            {
+                fontNames.Add(t.font.ToString());
+            }
+        }
+        return fontNames;
+    }
+}
